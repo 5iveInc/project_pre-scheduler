250b88fbf9eda48567373b5107e5efe67ac5997c
kanemitsu_5ive <kanemitsu@5ive.jp>
2026-04-01 13:44:16 +0900

    Basic認証機能（Vercelページのみ）追加

---
+++ project/middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  if (process.env.NODE_ENV !== 'production') {
+    return NextResponse.next()
+  }
+
+  const basicAuth = req.headers.get('authorization')
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, password] = atob(authValue).split(':')
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      password === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next()
+    }
+  }
+
+  return new NextResponse('Unauthorized', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"',
+    },
+  })
+}
+
+export const config = {
+  matcher: ['/((?!_next|favicon.ico).*)'],
+}
